2080a9d2a6688711b412afc14c3bebece0d06b91
Tanishq Sharma <tanishqrajasharma@gmail.com>
2025-07-04 11:10:41 +0530

    axios now sends cookies with every req

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -7,3 +7,16 @@ export const axiosInstance = axios.create({
     withCredentials: true,
 
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // or sessionStorage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export { axiosInstance };
