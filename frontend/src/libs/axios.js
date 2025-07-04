9074e647fd411eff4087f9c75355e5013edc7b76
Tanishq Sharma <tanishqrajasharma@gmail.com>
2025-07-04 11:11:42 +0530

    axios.js updated

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
     baseURL : import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : "https://whey-code-backend.onrender.com/api/v1",
     withCredentials: true,
 
